fe317529c1691f847d6f88f20f0208a9ceba52ae
Cristiano <cristiano.macedo@souunit.com.br>
2020-09-14 20:11:59 -0300

    Adicionar arquivos de projeto.

---
+++ AlgoritmoIrmaoMaior/IrmaoMaior.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoritmoIrmaoMaior
+{
+    public class IrmaoMaior
+    {
+        public int Obter(int n)
+        {
+            if (n > 100000000) return -1;
+
+            var listaNumeros = n.ToString().ToCharArray();
+            Array.Sort(listaNumeros);
+            Array.Reverse(listaNumeros);
+
+            var valorFinal = Convert.ToInt32(String.Join("", listaNumeros));
+            return valorFinal < 100000000 ? valorFinal : -1;
+        }
+    }
+}
---
+++ AlgoritmoIrmaoMaior/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AlgoritmoIrmaoMaior
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var IrmaoMaior = new IrmaoMaior();
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Digite um valor positivo (-1 para finalizar): ");
+
+                    int n = Convert.ToInt32(Console.ReadLine());
+
+                    if (n < 0)
+                        break;
+
+                        Console.WriteLine($"Irmao maior: {IrmaoMaior.Obter(n)}");
+                }
+                catch
+                {
+                    Console.WriteLine("Valor incorreto");
+                }
+            }
+        }
+    }
+}
